02b0c9e079a10c2471dae52c6739aa469f597d71
takshakghagi <140975941+takshakghagi@users.noreply.github.com>
2024-02-17 16:32:15 +0530

    Add files via upload

---
+++ first.java
@@ -0,0 +1,15 @@
+
+import javax.swing.*;
+
+public class first extends JFrame{
+    
+    first(){
+        setSize(1200, 600);
+        setLocation(100,100);
+        setVisible(true);
+    }
+    
+    public static void main(String[]args){
+        new first();
+    }
+}
